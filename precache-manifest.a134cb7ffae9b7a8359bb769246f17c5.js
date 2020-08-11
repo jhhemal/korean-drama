caa6e111961db81bde4508022f59a9688e39d01a
Jahidul Hasan <jahidulhasanhemal@gmail.com>
2020-08-11 17:00:15 +0600

    Updates

---
+++ precache-manifest.a134cb7ffae9b7a8359bb769246f17c5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d865e2c16aa1b97da381d32ee70df86",
+    "url": "/korean-drama/index.html"
+  },
+  {
+    "revision": "8880e5199a6c1ecd0892",
+    "url": "/korean-drama/static/css/main.f668b5d4.chunk.css"
+  },
+  {
+    "revision": "65b23f4723741dc3e192",
+    "url": "/korean-drama/static/js/2.562117a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/korean-drama/static/js/2.562117a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8880e5199a6c1ecd0892",
+    "url": "/korean-drama/static/js/main.7038591a.chunk.js"
+  },
+  {
+    "revision": "6c3811b1dde976ea3fcb",
+    "url": "/korean-drama/static/js/runtime-main.f9bd620e.js"
+  },
+  {
+    "revision": "44dfa63cfd53f4b696682975c376d671",
+    "url": "/korean-drama/static/media/itokay.44dfa63c.jpg"
+  },
+  {
+    "revision": "d4a3766e2dd1f90a8dc6da1702248570",
+    "url": "/korean-drama/static/media/pinocchio.d4a3766e.png"
+  },
+  {
+    "revision": "520e88e3546c80c21c02cb213f759ec0",
+    "url": "/korean-drama/static/media/rosy.520e88e3.jpg"
+  },
+  {
+    "revision": "38c84df19d352e1c252c2f181e422108",
+    "url": "/korean-drama/static/media/stay.38c84df1.jpg"
+  },
+  {
+    "revision": "360fe71494a45410ee8ba3493a46227f",
+    "url": "/korean-drama/static/media/strang.360fe714.jpg"
+  }
+]);
